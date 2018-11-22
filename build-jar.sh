9d571b0741abc83e3228d88090e6ce0980a1e228
Javi Armendáriz <katanagari@gmail.com>
2018-11-22 12:38:46 +0100

    Fixed compilation and jar build script

--- build-jar.sh
+++ build-jar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Make sure that you're using javac 8
-javac -cp lib/*.jar org/apache/logging/log4j/LogManager.java org/apache/logging/log4j/util/StackLocator.java -d output
+javac org/apache/logging/log4j/LogManager.java org/apache/logging/log4j/util/StackLocator.java -cp lib/*.jar -d output
 
-jar cvf output/log4j-api-no-mrjar-2.11.1.jar META-INF/NOTICE output/org
+jar cvf output/log4j-api-no-mrjar-2.11.1.jar META-INF/NOTICE -C output org
